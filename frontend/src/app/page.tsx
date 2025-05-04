559b719ea0d2f21ee85344ddf5ce346047c9ec9b
aniakarpliuk21 <aniakarpluk21@gmail.com>
2025-05-04 18:23:50 +0300

    с2

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import LoginComponent from "@/components/login.component";
+
+const HomePage = () => {
+  return (
+      <div>
+        <LoginComponent />
+      </div>
+  );
+};
+
+export default HomePage;
+
